d38f0591165e11edba1e3380c102e97c11f4deb8
nolan595 <98029345+nolan595@users.noreply.github.com>
2023-11-29 23:41:50 +0000

    Added Cors Policy

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const corsOptions = {
   origin:
     "https://6567c3bad5c1e22bc0c2d0d4--effulgent-pavlova-6e08dd.netlify.app",
-  optionsSuccessStatus: 200, // For legacy browser support
+  optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
